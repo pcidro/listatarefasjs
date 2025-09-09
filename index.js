1af64cbd5a4e4a41f57c9a49c81fa9bd9820d3ab
Paulo Cidro <paulocidro@gmail.com>
2025-09-09 10:04:02 -0300

    lista-tarefas

--- index.js
+++ index.js
@@ -4,41 +4,77 @@ const lista = document.getElementById("lista-container");
 const paragrafoinfo = document.getElementById("paragrafo-info");
 const totalTexto = document.getElementById("total-tarefas");
 
-// Atualizar o total de tarefas
-function atualizarTotal() {
+botao.addEventListener("click", adcionarTarefa);
+
+function atualizarTarefa() {
   const total = lista.children.length;
-  totalTexto.innerText = `Total: ${total} ${
-    total === 1 ? "tarefa" : "tarefas"
-  }`;
-  totalTexto.style.display = total > 0 ? "block" : "none";
-  paragrafoinfo.style.display = total > 0 ? "none" : "block";
+
+  if (total === 1) {
+    totalTexto.innerText = `Total: ${total} tarefa`;
+  } else {
+    totalTexto.innerText = `Total: ${total} tarefas`;
+  }
+
+  if (total > 0) {
+    totalTexto.style.display = "block";
+    paragrafoinfo.style.display = "none";
+  } else {
+    totalTexto.style.display = "none";
+    paragrafoinfo.style.display = "block";
+  }
+  salvarTarefas();
 }
 
-// Função adcionar tarefa / remover
-function callBackBotao() {
+function adcionarTarefa(event) {
+  event.preventDefault();
   const valor = input.value.trim();
   if (valor === "") return;
 
-  const li = document.createElement("li");
-  li.classList.add("item-tarefa");
-
-  const texto = document.createTextNode(valor);
-  li.appendChild(texto);
-
-  const botaoExcluir = document.createElement("button");
-  botaoExcluir.innerHTML = "🗑️";
-  botaoExcluir.classList.add("botao-excluir");
-
-  botaoExcluir.addEventListener("click", () => {
-    li.remove();
-    atualizarTotal();
-  });
-
-  li.appendChild(botaoExcluir);
-  lista.appendChild(li);
-
+  criarTarefa(valor);
   input.value = "";
-  atualizarTotal();
+  atualizarTarefa();
 }
 
-botao.addEventListener("click", callBackBotao);
+function criarTarefa(textoTarefa) {
+  const li = document.createElement("li");
+  li.classList.add("item-tarefa");
+  li.textContent = textoTarefa;
+
+  const botaoDelete = document.createElement("button");
+  botaoDelete.classList.add("botao-excluir");
+  const icone = document.createElement("i");
+  icone.classList.add("fa-solid", "fa-trash");
+  botaoDelete.appendChild(icone);
+  botaoDelete.addEventListener("click", () => {
+    li.remove();
+    atualizarTarefa();
+  });
+
+  li.appendChild(botaoDelete);
+  lista.appendChild(li);
+}
+
+function salvarTarefas() {
+  const itens = Array.from(lista.querySelectorAll("li")).map((li) =>
+    li.firstChild.textContent.trim()
+  );
+
+  localStorage.setItem("tarefas", JSON.stringify(itens));
+}
+
+// ====== CARREGAR do localStorage ======
+function carregarTarefas() {
+  const dados = localStorage.getItem("tarefas");
+  if (!dados) return;
+
+  const itens = JSON.parse(dados);
+
+  itens.forEach((texto) => {
+    criarTarefa(texto);
+  });
+
+  atualizarTarefa();
+}
+
+// chama ao abrir a página
+carregarTarefas();
